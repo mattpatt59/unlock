2d57f5c73716d7ee08ec8e72001addcd1a28c99f
Matthieu Patteyn <matthieu.patteyn@mirsense.com>
2020-04-04 00:53:39 +0200

    Les cartes n'arrivent plus en double , si la carte n'existe pas , elle n'est pas affichée

--- site.js
+++ site.js
@@ -1,4 +1,86 @@
+function fileExists(dir, successCallback, errorCallback) {
+    var xhttp = new XMLHttpRequest;
+
+    /* Check the status code of the request */
+    xhttp.onreadystatechange = function() {
+        return (xhttp.status !== 404) ? successCallback : errorCallback;
+    };
+
+    /* Open and send the request */
+    xhttp.open('head', dir, false);
+    xhttp.send();
+};
+
+function extractUrlParams(){	
+	var t = location.search.substring(1).split('&');
+	var f = [];
+	for (var i=0; i<t.length; i++){
+		var x = t[ i ].split('=');
+		f[x[0]]=x[1];
+	}
+	return f;
+}
+
+
+function urlExists(testUrl) {
+    var http = jQuery.ajax({
+        type:"HEAD", //Not get
+        url: testUrl,
+        async: false
+    })
+    return http.status!=404;
+}
+
 var url_images="";
+var url_website="https://cippil.fr/unlock/";
+
+//////////////////////////////////////////////////////////////////////////////////
+//Cards 
+
+
+function hide_card(id){
+	document.getElementById(id+'_card').remove()
+}
+
+function add_card(id){
+	//remove card if exist 
+	
+	try {
+		hide_card(id);
+	}
+	catch(error) {
+		
+	}
+	
+	
+	if(urlExists(url_website+"/"+url_images+"/" + id + ".png")){
+		var card="<table id=\"" + id + "_card\" style=\"display:inline\">";
+		card+="<tr><td><img src=\"" + url_images + "/" + id + ".png\"></td></tr>";
+		card+="<tr><td><a onclick=\"hide_card('" + id + "');\">Ranger</a></td></tr></table>";
+		document.getElementById('card_zone').innerHTML += card;
+	}
+	else{
+		alert("carte inconnue");
+	}
+	
+
+}
+
+
+
+function find_card(){
+	add_card(document.getElementById('text_card').value)
+}
+
+$(document).on("keypress", "input", function(e){
+	if(e.which == 13){
+		find_card();
+	}
+});
+
+////////////////////////////////////////////////////////////////////////////////////
+//Aventure 
+
 
 function select_aventure(aventure){
 	//global url_images;
@@ -15,38 +97,4 @@ function select_aventure(aventure){
 		add_card("welcome1");
 	}
 	
-}
-
-
-function add_card(id){
-	//global url_images;
-	var card="<table id=\""+id+"_card\" style=\"display:inline\">";
-	card+="<tr><td><img src=\""+url_images+"/" + id + ".png\"></td></tr>";
-	card+="<tr><td><a onclick=\"hide_card('"+id+"');\">Supprimer</a></td></tr></table>";
-	
-	document.getElementById('card_zone').innerHTML += card;
-}
-
-function hide_card(id){
-	document.getElementById(id+'_card').remove()
-}
-
-function find_card(){
-	add_card(document.getElementById('text_card').value)
-}
-
-$(document).on("keypress", "input", function(e){
-	if(e.which == 13){
-		find_card();
-	}
-});
-
-function extractUrlParams(){	
-	var t = location.search.substring(1).split('&');
-	var f = [];
-	for (var i=0; i<t.length; i++){
-		var x = t[ i ].split('=');
-		f[x[0]]=x[1];
-	}
-	return f;
 }
